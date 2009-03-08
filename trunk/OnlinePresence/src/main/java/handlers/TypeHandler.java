27aae1216777afd9b9afc8b2629a99ffea9c6e69
nikola.milikic <nikola.milikic@8602283e-b552-0410-b444-3b0cfedc99d3>
2009-03-08 17:42:14 +0000

    Share project "opo" into "https://onlinepresence.googlecode.com/svn"
    Maven is now included with the project. 
    
    NO TESTS ARE INCLUDED YET!!!

---
+++ trunk/OnlinePresence/src/main/java/handlers/TypeHandler.java
@@ -0,0 +1,43 @@
+/**
+* @author: Nikola Milikic
+* e-mail: nikola.milikic@gmail.com
+* @date: Aug 9, 2008
+* @version: 0.1
+*/
+package handlers;
+
+import presence.OnlinePresence;
+
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+/**
+ * @author Nikola Milikic
+ *
+ */
+public class TypeHandler implements AbstractHandler {
+
+	/* (non-Javadoc)
+	 * @see handlers.AbstractHandler#handleNode(presence.OnlinePresence, com.hp.hpl.jena.rdf.model.RDFNode)
+	 */
+	
+	public void handleNode(OnlinePresence oPresence, Resource subject, RDFNode object){
+		if(!subject.isAnon()){
+			String objectString = object.toString();
+			String className = null;
+			
+			if(objectString.contains("#"))
+				className = objectString.substring(objectString.indexOf("#") + 1);
+			else
+				className = objectString.substring(objectString.lastIndexOf("/") + 1);
+			
+			System.out.println(subject.toString());
+			
+			if(className.equals("OnlinePresence"))
+				oPresence.setURI(subject.toString());
+			else
+				oPresence.getObjectProperty(className).setURI(subject.toString());
+		}
+	}
+
+}
---
+++ trunk/OnlinePresence/src/main/java/opoExporter/OPOImporter.java
@@ -0,0 +1,103 @@
+/**
+ * @author: Nikola Milikic
+ * e-mail: nikola.milikic@gmail.com
+ * @date: Jul 29, 2008
+ * @version: 0.1
+ */
+package opoExporter;
+
+import handlers.AbstractHandler;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+import presence.OnlinePresence;
+
+/**
+ * @author Nikola Milikic
+ * 
+ */
+public class OPOImporter {
+
+	
+	public OnlinePresence importRDF(String fileName) throws FileNotFoundException, IOException, InvalidPropertiesFormatException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		OnlinePresence op = new OnlinePresence();;
+		Model model = null;
+		
+		Properties opoProperties = OPOImporter.readXmlProperties("conf/opoProperties.xml");
+		model = getModelFromRDF(fileName);
+		
+		if(model != null){
+
+			StmtIterator iter = model.listStatements();
+			
+			while(iter.hasNext()){
+				
+				Statement stat = iter.nextStatement();
+				String pred = stat.getPredicate().toString();
+				
+				AbstractHandler handler = (AbstractHandler) Class.forName(opoProperties.getProperty(pred)).newInstance();
+					
+				handler.handleNode(op, stat.getSubject(), stat.getObject());
+			}
+		}
+		
+		return op;
+	}
+	
+	public static Properties readXmlProperties(String filePath) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
+		Properties prop = new Properties();
+		
+		prop.loadFromXML(ClassLoader.getSystemResourceAsStream(filePath));
+		return prop;
+	}
+
+	public Model getModelFromRDF(String fileName) throws IOException, FileNotFoundException{
+		Model m = null;
+		
+		InputStream is = new BufferedInputStream(new FileInputStream(
+				fileName));
+		m  = ModelFactory.createDefaultModel().read(new InputStreamReader(is), "http://ggg.milanstankovic.org/opo/ns#", "RDF/XML");
+		is.close();
+		return m;
+	}
+	
+	public static void main(String[] args) {
+		//Ove testove prebaciti u odgovarajuce JUnit metode u folderu test
+		OPOImporter oim = new OPOImporter();
+		
+		OnlinePresence o = null;
+		try {
+			o = oim.importRDF("works.rdf");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (InvalidPropertiesFormatException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		OPOExporter oex = new OPOExporter(o);
+		oex.makeModel();
+
+		oex.serializeToXMLRDF("worksfine.rdf");
+
+	}
+}
